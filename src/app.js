19485ec196d7f7766f177ec6c3fb38904697156c
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-09-24 23:18:24 +0100

    added function from previous code

--- src/app.js
+++ src/app.js
@@ -1,24 +1,56 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// 
-// import 'bulma';
-// import './scss/style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <main>
-//         <section>
-//           <div className="container">
-//             <h1>Test</h1>
-//           </div>
-//         </section>
-//       </main>
-//     );
-//   }
-// }
+import './scss/main.scss';
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  getClassification(integer) {
+    var i = 1,
+      aliquotSum = 0;
+    while(i < integer){
+      if (integer % i === 0)
+        aliquotSum = aliquotSum + i;
+      i++;
+    }
+    if (aliquotSum < integer) return `${integer} is a deficient number`;
+    else if (aliquotSum > integer) return `${integer} is a abundant number`;
+    return `${integer} is a perfect number`;
+  }
+
+  // handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const result = this.getClassification(this.state.input);
+  //   this.setState({result});
+  // };
+
+  // handleChange = ({ target: { name, value }}) => {
+  //   this.setState({ [name]: value });
+  // };
+
+  render() {
+    console.log(this.state);
+    return (
+      <main>
+        <div className="content">
+          <h1>Check whether a number is Perfect, Abundant, or Deficient</h1>
+          {/* <form>
+              <label>Please enter a number</label>
+              <input type="text" className="input" />
+              <button type="submit" name="button">Submit</button>
+            </form>
+            <h2 className="result"></h2> */}
+        </div>
+      </main>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
