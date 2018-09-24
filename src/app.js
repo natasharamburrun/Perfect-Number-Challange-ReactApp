38d5d72a35eae4f14c4878f973b5cf73f03e078a
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-09-24 21:12:52 +0100

    addded react dependancies and files

---
+++ src/app.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// 
+// import 'bulma';
+// import './scss/style.scss';
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <main>
+//         <section>
+//           <div className="container">
+//             <h1>Test</h1>
+//           </div>
+//         </section>
+//       </main>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
